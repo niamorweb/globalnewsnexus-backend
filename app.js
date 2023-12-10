1d69a73fe1d5921850cd75b5ccb391d1fb277320
romain <niamor.lth@gmail.com>
2023-12-10 20:10:58 +0100

    update cors

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-const cors = require('cors');
-app.use(cors());
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "https://globalnewsnexus-frontend.vercel.app/",
+    optionsSuccessStatus: 200,
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
